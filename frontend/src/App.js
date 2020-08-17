065210f60a09b7291f2507906c534a248460c9fe
Vaibhav Jindal <vaibhav.jindal.2001@gmail.com>
2020-08-18 00:23:15 +0530

    add apollo client

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import React from "react";
+import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
+import apolloClient from "./apollo/ApolloClient";
 import Header from "./components/Header";
 import Routes from "./Routes";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes />
-      </div>
-    </BrowserRouter>
+    <ApolloProvider client={apolloClient}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Routes />
+        </div>
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
